f93cfe45ee2ab96c52bed2161b07c8aafeeb7fb6
Anjitha N <127217616+AnjithaIxico@users.noreply.github.com>
2023-09-23 22:24:42 +0000

    Copilot third  commit

---
+++ comments.js
@@ -0,0 +1,84 @@
+// Create web server
+// 1. npm init -y
+// 2. npm install express
+// 3. npm install nodemon
+// 4. npm install ejs
+// 5. npm install body-parser
+// 6. npm install mongoose
+
+// 7. npm run devStart
+// 8. npm install method-override
+// 9. npm install express-sanitizer
+// 10. npm install connect-flash
+// 11. npm install express-session
+// 12. npm install passport
+// 13. npm install passport-local
+// 14. npm install passport-local-mongoose
+
+// 15. npm install --save multer
+// 16. npm install cloudinary
+// 17. npm install dotenv
+// 18. npm install connect-mongo
+
+// 19. npm install moment
+// 20. npm install moment-timezone
+
+// 21. npm install mongoose-findorcreate
+
+// 22. npm install mongoose-paginate-v2
+
+// 23. npm install mongoose-paginate
+
+// 24. npm install mongoose-paginate-v2
+
+// 25. npm install mongoose-paginate-v2
+
+// 26. npm install multer-gridfs-storage
+
+// 27. npm install mongoose-gridfs
+
+// 28. npm install multer-gridfs-storage
+
+// 29. npm install morgan
+
+// 30. npm install multer
+
+// 31. npm install multer-gridfs-storage
+
+// 32. npm install mongoose-gridfs
+
+// 33. npm install express-handlebars
+
+// 34. npm install method-override
+
+// 35. npm install express-session
+
+// 36. npm install passport
+
+// 37. npm install passport-local
+
+// 38. npm install passport-local-mongoose
+
+// 39. npm install connect-flash
+
+// 40. npm install dotenv
+
+// 41. npm install connect-mongo
+
+// 42. npm install moment
+
+// 43. npm install moment-timezone
+
+// 44. npm install mongoose-findorcreate
+
+// 45. npm install mongoose-paginate-v2
+
+// 46. npm install mongoose-paginate
+
+// 47. npm install mongoose-paginate-v2
+
+// 48. npm install mongoose-paginate-v2
+
+// 49. npm install mongoose-paginate-v2
+
+// 50. npm install mongoose-paginate
